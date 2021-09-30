1945399ae47ae9a940851dd21a1d02dbebaf67b8
jbuendia1y <jgamer669@gmail.com>
2021-09-30 12:39:15 -0500

    Resolved #1

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import Button from "@/components/Button";
+import ReactIcon from "./components/Icons/ReactIcon";
+
+function App() {
+  return (
+    <div className="App ">
+      <div className="container">
+        <Button>
+          <ReactIcon />
+          Logo
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
