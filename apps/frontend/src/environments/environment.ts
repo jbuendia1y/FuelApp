374ffdfc0710a98963602e96c49547a29902ca2a
Joaquín Buendía <jgamer669@gmail.com>
2022-01-12 17:56:25 -0500

    SERVER_BASE_URL

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@ const ENTERPRISE_NAME = process.env.ENTERPRISE_NAME
 
 export const environment = {
   ENTERPRISE_NAME,
-  SERVER_BASE_URL: SERVER_BASE_URL ? SERVER_BASE_URL : "http://localhost:8000/api",
+  SERVER_BASE_URL: SERVER_BASE_URL ? SERVER_BASE_URL : "http://localhost:8000",
 
   TOKEN_FIELD_NAME: "token",
   USER_FIELD_NAME: "user",
