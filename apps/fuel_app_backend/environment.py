ef887aca299cd37458320473bef39a33f86b066d
Joaquín Buendía <jgamer669@gmail.com>
2022-01-15 12:42:42 -0500

    Error de escritura y environment

--- apps/fuel_app_backend/environment.py
+++ apps/fuel_app_backend/environment.py
@@ -1,5 +1,8 @@
 import os
 
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "SQLALCHEMY_DATABASE_URL", "sqlite:///./sql_app.db")
+
 JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 JWT_MINUTES_EXPIRES = int(os.environ.get("JWT_MINUTES_EXPIRES", "15"))
